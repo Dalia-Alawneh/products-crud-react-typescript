61e6b1287f0bd40a27595e7eeaf9c9d85fcc0578
Dalia Alawneh <alawnehdalia340@gmail.com>
2023-11-08 22:11:21 +0200

    Add jsDoc for textSlicer function

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,3 +1,12 @@
+/**
+ * 
+* @param {string} txt - The text string to be sliced.
+* @param {number} [max = 50] - The maximum length of the sliced text. Defaults to 50 if not provided.
+* @returns {string} - The sliced text string.
+* Slices a text string to a specified maximum length.
+If the text length is greater than or equal to the maximum length, it truncates the text and adds ellipsis at the end.
+If the maximum length is not provided, it defaults to 50.
+*/
 export const textSlicer = (txt: string, max: number = 50) => {
     if (txt.length >= max) {
         return `${txt.slice(0, max)} ...`
