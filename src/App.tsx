f37999d68159d4a7087fe6bfa7c02b9e169cbf54
Dalia Alawneh <alawnehdalia340@gmail.com>
2023-11-08 22:36:30 +0200

    Update card design

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ function App() {
   const renderProductList = productList.map(product => <ProductCard product={product} key={product.id}/>)
 
   return (
-    <>
-    <div className='grid items-stretch grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 p-3 gap-2'>
+    <main className='container'>
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 p-3 gap-2 md:gap-4'>
       {renderProductList}
     </div>
-    </>
+    </main>
   )
 }
 
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -4,38 +4,37 @@ import Image from "./Image.tsx"
 import Button from "./ui/Button.tsx"
 
 interface IProps {
-    product:IProduct;
+    product: IProduct;
 }
 const ProductCard = ({ product }: IProps) => {
-    const {title, description, imageURL, price} = product
+    const { title, description, imageURL, price, category } = product
     return (
-        <div>
-            <div className="border-2 rounded-md p-2 flex flex-col">
-                <Image className="rounded-md"
+        <div className="max-w-sm mx-auto md:mx-0 border-2 rounded-md p-2 flex flex-col">
+            <Image className="rounded-md h-52 w-full lg:object-cover"
                 imageUrl={imageURL}
-                    alt={title}
-                />
-                <h3>{title}</h3>
-                <p>{textSlicer(description,70)}</p>
-                <div className="flex items-center space-x-2 my-5">
-                    <span className="w-5 h-5 rounded-full bg-red-300" />
-                    <span className="w-5 h-5 rounded-full bg-red-300" />
-                    <span className="w-5 h-5 rounded-full bg-red-300" />
-                    <span className="w-5 h-5 rounded-full bg-red-300" />
-                </div>
-                <div className="flex justify-between items-center">
-                    <span>${price}</span>
-                    <Image className="w-10 h-10 rounded-full object-center"
-                    imageUrl="https://images.unsplash.com/photo-1583121274602-3e2820c69888?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                alt={title}
+            />
+            <h3>{title}</h3>
+            <p>{textSlicer(description, 70)}</p>
+            <div className="flex items-center space-x-2 my-5">
+                <span className="w-5 h-5 rounded-full bg-red-300" />
+                <span className="w-5 h-5 rounded-full bg-red-300" />
+                <span className="w-5 h-5 rounded-full bg-red-300" />
+                <span className="w-5 h-5 rounded-full bg-red-300" />
+            </div>
+            <div className="flex justify-between items-center">
+                <span className="text-indigo-400 font-bold">${price}</span>
+                <Image className="w-10 h-10 rounded-full object-center"
+                    imageUrl={category.imageURL}
                     alt="name"
                 />
-                </div>
-                <div className="flex space-x-2 mt-5">
-                    <Button className="bg-indigo-700" width="w-full">Edit</Button>
-                    <Button className="bg-red-700" width="w-full">Delete</Button>
-                </div>
+            </div>
+            <div className="flex space-x-2 mt-5">
+                <Button className="bg-indigo-700" width="w-full">Edit</Button>
+                <Button className="bg-red-700" width="w-full">Delete</Button>
             </div>
         </div>
+
     )
 }
 
