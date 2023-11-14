5134fbcd5f6b0246b119e41c86a6421ed5039075
Dalia Alawneh <alawnehdalia340@gmail.com>
2023-11-14 15:47:47 +0200

    Fix node version bug

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['uuid'],
+    },
+    target: [
+      'node14',
+      'node > 16'
+    ],
+  },
 })
